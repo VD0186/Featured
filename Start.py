05ca7fce7c1c02b93f190706ca34c97fe08c342f
VD0186 <114549356+VD0186@users.noreply.github.com>
2025-03-31 13:27:38 +0800

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,1 +1,22 @@
+age=int(input("請輸入年齡"))
+print("(1)男(2)女")
+gender=int(input("性別:"))
+print("(1)定期(2)終身")
+date=int(input("壽險類型:"))
+if age<30:
 
+if else age<65:
+
+else <=85:
+
+if gender==1:
+  G='男'
+else :
+  G='女'
+if date==1:
+  D='定期'
+else :
+  D='長期'
+print("年齡：",age,F,D)
+
+  
